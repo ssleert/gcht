e1a34494c361bb190e49526a679e0ef30e817cff
ssleert <ryasempav@technologysolutionsgeorgia.ge>
2024-10-16 20:01:10 +0300

    swag

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
 import { logger } from "hono/logger";
-import { bodyLimit } from "hono/body-limit";
 
 let idx = 1;
 const chat = [] as {
@@ -15,31 +14,22 @@ app.use(logger());
 
 app.get("/", serveStatic({ path: "./index.html" }));
 
-app.post(
-  "/api/new_message",
-  bodyLimit({
-    maxSize: 10 * 1024, // 50kb
-    onError: (c) => {
-      return c.text("overflow :(", 413);
-    },
-  }),
-  async (c) => {
-    const data = await c.req.json() as { m: string; u: string };
+app.post("/api/new_message", async (c) => {
+  const data = await c.req.json() as { m: string; u: string };
 
-    if (data.u == "") {
-      data.u = "unknown";
-    }
-    chat.push({
-      u: data.u,
-      m: data.m,
-      i: idx++,
-    });
+  if (data.u == "") {
+    data.u = "unknown";
+  }
+  chat.push({
+    u: data.u,
+    m: data.m,
+    i: idx++,
+  });
 
-    console.log(chat);
+  console.log(chat);
 
-    return c.body(null, 200);
-  },
-);
+  return c.body(null, 200);
+});
 
 app.get("/api/get_messages/:since", (c) => {
   const { since } = c.req.param();
